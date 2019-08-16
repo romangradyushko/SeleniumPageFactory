dc36a32271f58ac5bb86925ae1b8dd3f935b8c13
romangradyushko <romangradyushko@gmail.com>
2019-08-16 04:41:15 +0300

    some new fix(assert)

--- src/main/java/com/epam/AbstractPage.java
+++ src/main/java/com/epam/AbstractPage.java
@@ -1,13 +1,11 @@
 package com.epam;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.PageFactory;
 
 public class AbstractPage {
 	
 	protected WebDriver driver;
 	public AbstractPage(WebDriver driver){
         this.driver = driver;
-        PageFactory.initElements(driver, this);
 	}
 }
--- src/test/java/com/epam/MailRuTest.java
+++ src/test/java/com/epam/MailRuTest.java
@@ -1,11 +1,14 @@
 package com.epam;
 
+
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import com.epam.MailBoxPage;
+
 
 public class MailRuTest {
 	private static final String BASE_URL = "https://mail.ru/";
@@ -15,50 +18,28 @@ public class MailRuTest {
 	private static final String SUBJECT = "Test";
 	private static final String BODY_TEXT = "zvsvsvTest";
     private WebDriver driver;
-	private HomePage HomePage;
-	private MailPage MailPage;
-	private DraftPage DraftPage;
-	private SentMailPage SentMailPage;
+	private MailBoxPage mailBoxPage;
 	
     @BeforeClass
     public void setUpBefore(){
     	String exePath = "D:\\3lvl\\EPAM QA\\selenium\\Chrome driver\\chromedriver.exe";
 		System.setProperty("webdriver.chrome.driver", exePath);		
-		driver = new ChromeDriver();
-    	HomePage = new HomePage(driver);
-    	MailPage = new MailPage(driver);
-    	DraftPage = new DraftPage(driver);
-    	SentMailPage = new SentMailPage(driver);
-    }
-    
-    @AfterClass
-    public void setUpAfter(){
-    	driver.quit();
     }
     
     @Test
     public void loginMailRu() {
+    	driver = new ChromeDriver();
+    	mailBoxPage = new MailBoxPage(driver);
         driver.get(BASE_URL);
-        Assert.assertEquals(driver.getTitle(), "Mail.ru: почта, поиск в интернете, новости, игры", "Incorrected title");
-    	HomePage.loginMailRu(LOGIN, PASSWORD);
-    	Assert.assertEquals(driver.getTitle(), "Входящие - Почта Mail.ru", "Incorrected title");
-    }
-    
-    @Test
-    public void saveMail() {
-    	MailPage.createAndSaveNewEmail(EMAIL_ADDRESS, SUBJECT, BODY_TEXT);
-    	Assert.assertEquals(driver.getTitle(), "Входящие - Почта Mail.ru", "Incorrected title");
-    }
-    
-    @Test
-    public void verifyMail() {
-    	DraftPage.verifyMailInDraft(EMAIL_ADDRESS, SUBJECT, BODY_TEXT);
-    	Assert.assertEquals(driver.getTitle(), "Черновики - Почта Mail.ru", "Incorrected title");
-    }
-    
-    @Test
-    public void verifySentMail() {
-    	SentMailPage.isDraftMailSentAndDeleteFromDraft(EMAIL_ADDRESS, SUBJECT, BODY_TEXT);
-    	SentMailPage.goOut();
+    	System.out.println(driver.getTitle());
+    	mailBoxPage.loginMailRu(LOGIN, PASSWORD);
+    	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    	mailBoxPage.createAndSaveNewEmail(EMAIL_ADDRESS, SUBJECT, BODY_TEXT);
+    	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    	mailBoxPage.goToDraft();
+    	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    	mailBoxPage.verifyMailInDraft(EMAIL_ADDRESS, SUBJECT, BODY_TEXT);
+    	mailBoxPage.isDraftMailSentAndDeleteFromDraft(EMAIL_ADDRESS, SUBJECT, BODY_TEXT);
+    	mailBoxPage.goOut();
     }
 }
